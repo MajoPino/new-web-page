741ee3b7829088e136fea86314bd8cf8ea02a1e5
MajoPino <majo12pino@gmail.com>
2024-12-02 07:15:24 -0500

    creation of the winter soldier webpage v1

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('factButton').addEventListener('click', () => {
+    const fact = document.getElementById('fact');
+    fact.textContent = "Bucky's transformation into the Winter Soldier was a pivotal moment in Marvel's comic history, symbolizing how heroes can be corrupted.";
+    fact.classList.remove('hidden');
+});
